2b3fa25bbc857731921daa265caf79166bd6b249
Sandip Khandelwal <31161019+sandippk@users.noreply.github.com>
2023-09-06 05:36:34 +0000

    Change for Sandip

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sandip Khandelwal",
+  title: "Senior Engineering Architect",
+  email: "sandip.khandelwal@gmail.com",
+  gitHub: "sandippk",
+  instagram: "",
+  linkedIn: "sandipkhandelwal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
